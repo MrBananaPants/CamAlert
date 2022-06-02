194a76f8f747ab01d4ac0d62f1423af05ea870d3
MrBananaPants <joran.vancoillie@gmail.com>
2022-06-02 08:55:16 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ APP = ['CamAlert.py']
 DATA_FILES = []
 OPTIONS = {
     'iconfile': 'media/icon.icns',
-    'plist': {'CFBundleShortVersionString': '0.2.0', },
+    'plist': {'CFBundleShortVersionString': '0.3.0', },
     'packages': ['rumps', 'bs4', 'requests', 'objc']
 }
 
